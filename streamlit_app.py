f3573e1e689985fed04c2ae3cd74c75e67040584
Saran Sankar <31500520+saran-sankar@users.noreply.github.com>
2022-05-25 20:43:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,7 @@ st.title('Streamlit App')
 st.text('Text')
 
 fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+fruit_list = fruit_list.set_index('Fruit')
+
+streamlist.multiselect('Pick fruits: ', list(fruit_list.names))
 st.dataframe(fruit_list)
