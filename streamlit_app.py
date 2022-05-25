209180a7a64767bc4b1652cb548362444371aa63
Saran Sankar <31500520+saran-sankar@users.noreply.github.com>
2022-05-25 20:37:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Streamlit App')
-
 st.text('Text')
+
+fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+st.dataframe(fruit_list)
