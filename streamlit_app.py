0e18a9dfb4faf860cf5312d3b01a27a1df97fef2
Saran Sankar <31500520+saran-sankar@users.noreply.github.com>
2022-05-25 20:51:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,6 @@ st.text('Text')
 fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 fruit_list = fruit_list.set_index('Fruit')
 
-st.multiselect('Pick fruits: ', list(fruit_list.index), ['Apple', 'Cantaloupe'])
-st.dataframe(fruit_list)
+fruits_selected = st.multiselect('Pick fruits: ', list(fruit_list.index), ['Apple', 'Cantaloupe'])
+fruits_to_show = fruit_list.loc[fruits_selected]
+st.dataframe(fruits_to_show)
